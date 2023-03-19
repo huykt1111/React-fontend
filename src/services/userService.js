af401b8a63adf1e9c9b1e367334d1817247c3cdf
huykt1111 <huylmht10@gmail.com>
2023-03-19 14:18:44 +0700

    Create Clinic

--- src/services/userService.js
+++ src/services/userService.js
@@ -80,6 +80,10 @@ const getAllDetailSpecialtyById = (data) => {
     return axios.get(`/api/get-detail-specialty-by-id?id=${data.id}&location=${data.location}`)
 }
 
+const createNewClinic = (data) => {
+    return axios.post(`/api/create-new-clinic`, data);
+}
+
 export {
     handleLoginApi,
     getAllUsers,
@@ -99,5 +103,6 @@ export {
     postVerifyBookAppointment,
     createNewSpecialty,
     getAllSpecialty,
-    getAllDetailSpecialtyById
+    getAllDetailSpecialtyById,
+    createNewClinic,
 }
